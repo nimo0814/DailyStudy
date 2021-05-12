eeaf55b9abd9f5a8bdb981855b6ab3d5086dd40e
戴雅丽 <daiyali@daiyalideMacBook-Pro.local>
2021-05-12 21:37:54 +0800

    0512更新

---
+++ PycharmProjects/pythonPractice/Practice/delayInput.py
@@ -0,0 +1,9 @@
+"""
+题目 暂停一秒输出。
+程序分析 使用 time 模块的 sleep() 函数。
+
+"""
+import time
+for i in range(4):
+    print(str(int(time.time()))[-2:])
+    time.sleep(1)
---
+++ PycharmProjects/pythonPractice/Practice/watchTime.py
@@ -0,0 +1,7 @@
+"""
+题目 暂停一秒输出，并格式化当前时间。
+"""
+import time
+for i in range(4):
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+    time.sleep(1)
